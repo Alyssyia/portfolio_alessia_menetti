fd4de19dca3455b5a2ce54f32698bf81832fdcb8
Marco <marco.cusenza.me@gmail.com>
2021-11-15 03:25:47 +0100

    try bugfix 2

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 $(document).on('scroll', function () {
     if ($(this).scrollTop() > $('#drop-menu').position().top) {
         document.getElementById("drop-menu").classList.add("fixed");
-        // document.getElementById("drop-menu").classList.add("ms_opacity_5");
+        document.getElementById("drop-menu").classList.add("ms_opacity_5");
     }
 });
 
@@ -19,13 +19,11 @@ $(document).on('scroll', function () {
 $(document).on('scroll', function () {
     if ($(this).scrollTop() > $('#jq-trigger').position().top) {
         document.getElementById("scroll-down").classList.add("ms_hidden");
-        // document.getElementById("drop-menu").classList.add("ms_opacity_5");
     }
 });
 
 $(document).on('scroll', function () {
     if ($(this).scrollTop() <= $('#scroll-down').position().top) {
         document.getElementById("scroll-down").classList.remove("ms_hidden");
-        // document.getElementById("drop-menu").classList.remove("ms_opacity_5");
     }
 });
