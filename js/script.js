0802b50343285a95b42ec8595a0de72af3dbc031
Marco <marco.cusenza.me@gmail.com>
2021-11-15 20:32:16 +0100

    scroll down transition js RIMOSSO JQUERY YEEE

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
-//MENU BUTTON FIXED ON SCROLL
-// $(document).on('scroll', function () {
-//     if ($(window).scrollTop() > $('#header').position().top) {
-//         document.getElementById("drop-menu-box").classList.add("position-fixed");
-//     }
-// });
-
-// $(document).on('scroll', function () {
-//     if ($(window).scrollTop() < $('#header').position().top) {
-//         document.getElementById("drop-menu-box").classList.remove("position-fixed");
-//     }
-// });
-
 
 //SCRITTA SCROLL DOWN SCOMPARE
-$(document).on('scroll', function () {
-    if ($(window).scrollTop() > $('#landing-page').position().top) {
-        document.getElementById("scroll-down").classList.add("ms_hidden");
-    }
-});
+document.addEventListener('scroll', function () {
 
-$(document).on('scroll', function () {
-    if ($(window).scrollTop() <= $('#scroll-down').position().top) {
-        document.getElementById("scroll-down").classList.remove("ms_hidden");
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        document.getElementById("scroll-down").className = "ms_hidden_0";
+    }
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("scroll-down").className = "ms_hidden_25";
+    }
+
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("scroll-down").className = "ms_hidden_50";
+    }
+
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        document.getElementById("scroll-down").className = "ms_hidden_75";
+    }
+
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("scroll-down").className = "ms_hidden_100";
     }
 });
