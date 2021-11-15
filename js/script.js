dd6a166a635f6b6d8eb1f38388e853e01bb6c606
Marco <marco.cusenza.me@gmail.com>
2021-11-15 20:40:00 +0100

    transizione 1 trigger

--- js/script.js
+++ js/script.js
@@ -1,24 +1,9 @@
-
 //SCRITTA SCROLL DOWN SCOMPARE
 document.addEventListener('scroll', function () {
 
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        document.getElementById("scroll-down").className = "ms_hidden_0";
-    }
-
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("scroll-down").className = "ms_hidden_25";
-    }
-
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("scroll-down").className = "ms_hidden_50";
-    }
-
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-        document.getElementById("scroll-down").className = "ms_hidden_75";
-    }
-
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        document.getElementById("scroll-down").className = "ms_hidden_100";
+        document.getElementById("scroll-down").classList.add("ms_hidden_100");
+    } else {
+        document.getElementById("scroll-down").classList.remove("ms_hidden_100");
     }
 });
