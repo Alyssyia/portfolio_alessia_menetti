1abce35e72a47d4b36012a61bfb55b7ec3dcc935
Marco <marco.cusenza.me@gmail.com>
2021-11-15 11:59:57 +0100

    RIMOSSO JQUERY per fixare menu - position sticky

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-
-//MENU BUTTON FIXED ON SCROLL
-$(document).on('scroll', function () {
-    if ($(this).scrollTop() > $('#drop-menu').position().top) {
-        document.getElementById("drop-menu").classList.add("fixed");
-        document.getElementById("drop-menu").classList.add("ms_opacity_5");
-    }
-});
-
-$(document).on('scroll', function () {
-    if ($(this).scrollTop() <= $('#header').position().top) {
-        document.getElementById("drop-menu").classList.remove("fixed");
-        // document.getElementById("drop-menu").classList.remove("ms_opacity_5");
-    }
-});
-
-
 // //SCRITTA SCROLL DOWN SCOMPARE
 // $(document).on('scroll', function () {
 //     if ($(this).scrollTop() > $('#jq-trigger').position().top) {
