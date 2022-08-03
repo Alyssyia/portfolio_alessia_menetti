845391f20093ca6d1afe104602cabe0eb372842f
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-08-03 19:05:33 +0200

    try play-pause mobile

--- js/script.js
+++ js/script.js
@@ -2,11 +2,25 @@
 setTimeout(() => window.scrollTo(0, 0), 100); //scrolla e va verso X = 0 e Y = 0 cioè in cima
 
 //SCRITTA SCROLL DOWN SCOMPARE
-document.addEventListener('scroll', function () {
+document.addEventListener("scroll", function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("scroll-down").classList.add("ms_hidden_100");
+  } else {
+    document.getElementById("scroll-down").classList.remove("ms_hidden_100");
+  }
+});
 
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("scroll-down").classList.add("ms_hidden_100");
+const videoboxArray = document.querySelectorAll(".ms_video_box");
+const iframeArray = document.querySelectorAll("iframe");
+
+videoboxArray.forEach((el, i) => {
+  el.addEventListener("click", () => {
+    const myFrame = iframeArray[i];
+    const video = myFrame.contentWindow.document.getElementsByTagName("video")[0];
+    if (video.paused == true) {
+      video.play();
     } else {
-        document.getElementById("scroll-down").classList.remove("ms_hidden_100");
+      video.pause();
     }
+  });
 });
